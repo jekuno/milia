e1be7438f58dd0ebea2ec1b645b519167ea3c447
Daudi Amani <dsaronin@gmail.com>
2011-10-09 22:11:43 -0700

    0.1.3

--- lib/milia/railtie.rb
+++ lib/milia/railtie.rb
@@ -5,13 +5,8 @@ module Milia
   class Railtie < Rails::Railtie
     initializer :after_initialize do
  
-        ActiveRecord::Base.send(:extend, Milia::Base)
-        ActionController::Base.send(:extend, Milia::Control)
-      # ActiveRecord::Base.on_load(:active_record) do
-      # end
-      # 
-      # ActionController::Base.on_load(:action_controller) do
-      # end
+        ActiveRecord::Base.send(:include, Milia::Base)
+        ActionController::Base.send(:include, Milia::Control)
 
     end
 
