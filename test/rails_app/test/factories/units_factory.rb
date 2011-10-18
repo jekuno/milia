ae6d7b06d8804247fde6438ce18e45ee763ad313
Daudi Amani <dsaronin@gmail.com>
2011-10-18 12:29:32 -0700

    factory - 1

--- test/rails_app/test/factories/units_factory.rb
+++ test/rails_app/test/factories/units_factory.rb
@@ -1,12 +1,33 @@
 FactoryGirl.define do
-  USERNAMES = %w(demarcus deshaun jemell jermaine jabari kwashaun musa nigel kissamu yona brendon terell treven tyrese adonys)
   
-  factory :user do
-    f.sequence( :email ) do |n|
-      ndex = n % USERNAMES.size
-      "#{USERNAMES[ndex]}#{n}@example.com"
+  class << self
+    USERNAMES = %w(demarcus deshaun jemell jermaine jabari kwashaun musa nigel kissamu yona brenden terell treven tyrese adonys)
+
+    def pick_name(n)
+      return USERNAMES[ (n % USERNAMES.size) ] + n.to_s
     end
     
   end
+  
+  factory :tenant do |f|
+    f.tenant_id   nil
+  end
+  
+  factory :user do |f|
+    f.tenant_id   nil
+    f.sequence( :email ) { |n| "#{pick_name(n)}@example.com" }
+    f.password  'MonkeyMocha'
+    f.password_confirmation { |u| u.password }
+  end  # user
+  
+  factory :author do |f|
+    f.sequence( :name ) { |n| "#{pick_name(n)}@example.com" }
+    f.association :user
+  end   # :author
+  
+  factory :calendar do |f|
+    
+  end   # calendar
+  
   
 end
