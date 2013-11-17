a3248d6edcf3c557f5c8d0457fd76a0ed079388d
daudi amani <dsaronin@gmail.com>
2013-11-17 02:07:49 -0800

    sample-milia-app tutorial doc added - 2

--- doc/sample.sh
+++ doc/sample.sh
@@ -76,7 +76,7 @@ gem "SystemTimer", :require => "system_timer", :platforms => :ruby_18
 gem "rack-timeout"
 gem 'rails_12factor'
 
-gem 'web-app-theme'
+gem 'web-app-theme', :git => 'git://github.com/dsaronin/web-app-theme.git'
 gem 'devise'
 gem 'milia', :git => 'git://github.com/dsaronin/milia.git', :branch => 'newdev'
 gem 'recaptcha', :require => "recaptcha/rails"
@@ -122,5 +122,69 @@ mv app/views/home/show.html.haml app/views/home/index.html.haml
 # over at the browser, refresh the page
 
 
-# tweaking the web-app-theme
+# tweaking the web-app-theme to correct for defaults
 rails g web_app_theme:assets
+
+# correct: app/views/layouts/application.html.haml line 6 to:
+= javascript_include_tag 'application'
+
+# correct: app/assets/stylesheets/web-app-theme/basic.css
+# around line 300, comment out the three lines below and
+# add following instead
+/*
+.form .fieldWithErrors .error {
+  color: red;
+}
+*/
+
+.form input.text_field, .form textarea.text_area {
+  width: 100%;
+  border-width: 1px;
+  border-style: solid;
+}
+
+.flash .message {
+    -moz-border-radius: 3px;
+    -webkit-border-radius: 3px;
+    border-radius: 3px;
+    text-align: center;
+    margin: 0 auto 15px;
+    color: white;
+    text-shadow: 0 1px 0 rgba(0, 0, 0, 0.3);
+  }
+  .flash .message p {
+    margin: 8px;
+  }
+  .flash .error, .flash .error-list, .flash .alert {
+    border: 1px solid #993624;
+    background: #cc4831 url("images/messages/error.png") no-repeat 10px center;
+  }
+  .flash .warning {
+    border: 1px solid #bb9004;
+    background: #f9c006 url("images/messages/warning.png") no-repeat 10px center;
+  }
+  .flash .notice {
+    color: #28485e;
+    text-shadow: 0 1px 0 rgba(255, 255, 255, 0.7);
+    border: 1px solid #8a9daa;
+    background: #b8d1e2 url("images/messages/notice.png") no-repeat 10px center;
+  }
+  .flash .error-list {
+    text-align: left;
+  }
+  .flash .error-list h2 {
+    font-size: 16px;
+    text-align: center;
+  }
+  .flash .error-list ul {
+    padding-left: 22px;
+    line-height: 18px;
+    list-style-type: square;
+    margin-bottom: 15px;
+  }
+
+
+# move images for buttons to correct folder
+cp $(bundle show web-app-theme)/spec/dummy/public/images/* app/assets/images/web-app-theme/ -r
+
+
