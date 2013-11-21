c8946e452eaba516dc81a2c8224c5a2bc710450a
daudi amani <dsaronin@gmail.com>
2013-11-21 12:57:31 -0800

    milia control expanded with app ctlr basics - 7v

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,9 +1,15 @@
 module Milia
 
   class ConfirmationsController < Devise::ConfirmationsController
-    prepend_before_filter :require_no_authentication, :only => [ :new, :create, :show ]
 
+    before_action  :shout_out
     skip_before_action :authenticate_tenant!   #, :only => [:show, :new, :create]
+
+private
+  def shout_out
+  puts " ############ CONFIRMATIONS DEVISE CTLR ################### "
+  return true
+  end
 
   end  # class
 end # module
