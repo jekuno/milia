89c0fad405e83545db0f4e30b937656bcd5e3297
daudi amani <dsaronin@gmail.com>
2013-11-21 03:02:32 -0800

    milia control expanded with app ctlr basics - 6c

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -2,7 +2,7 @@ module Milia
 
   class ConfirmationsController < Devise::ConfirmationsController
 
-    skip_before_action :authenticate_tenant!, :only => [:show, :new, :create]
+    skip_before_action :authenticate_tenant!   #, :only => [:show, :new, :create]
 
   end  # class
 end # module
