594f2929d09970cf1858980783c28bcea11ce66a
daudi amani <dsaronin@gmail.com>
2013-11-23 21:52:20 -0800

    add invitable stuff - 5

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ module Milia
 
   class SessionsController < Devise::SessionsController
 
-    skip_before_action :authenticate_tenant!, :only => [:new, :create]
+    skip_before_action :authenticate_tenant!, :only => [:new, :create, :destroy]
 
   end  # class
 end # module
