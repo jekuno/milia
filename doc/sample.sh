3c13bd140f44e6caa10c1713c7a5c1db0a71aee9
daudi amani <dsaronin@gmail.com>
2013-12-01 01:37:50 -0800

    sample.sh - 2

--- doc/sample.sh
+++ doc/sample.sh
@@ -458,10 +458,16 @@ export RECAPTCHA_PRIVATE_KEY=6LeBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBgQBv
 
 # EDIT: config/routes.rb  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # ADD the :controllers clause to the existing devise_for :users  :
+
+  as :user do   #   *MUST* come *BEFORE* devise's definitions (below)
+    match '/user/confirmation' => 'milia/confirmations#update', :via => :put, :as => :update_user_confirmation
+  end
+
   devise_for :users, :controllers => { 
     :registrations => "milia/registrations",
+    :confirmations => "milia/confirmations",
     :sessions => "milia/sessions", 
-    :confirmations => "milia/confirmations" 
+    :passwords => "milia/passwords", 
   }
 
 #<<<< EDIT <<<<<<<<<<<<<<<<<
