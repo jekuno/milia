eaf38222e8e8357edbcd2682e3ece5742eec3a59
daudi amani <dsaronin@gmail.com>
2013-12-05 01:16:42 -0800

    sample.sh - 3

--- doc/sample.sh
+++ doc/sample.sh
@@ -420,7 +420,10 @@ export RECAPTCHA_PRIVATE_KEY=6LeBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBgQBv
 
 # EDIT: db/migrate/xxxxxxx_devise_create_users.rb >>>>>>>>>>>>>>>>>>>>>>>>>
 # add above the t.timestamps line:
-    t.references :tenant
+      # milia member_invitable
+      t.boolean    :skip_confirm_change_password, :default => false
+
+      t.references :tenant
 #<<<< EDIT <<<<<<<<<<<<<<<<<
 
 # generate the tenant migration
@@ -485,11 +488,11 @@ export RECAPTCHA_PRIVATE_KEY=6LeBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBgQBv
 # ADD after the class Tenant line: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     acts_as_universal_and_determines_tenant
 
-  def self.create_new_tenant(params)
+  def self.create_new_tenant(tenant_params, coupon_params)
 
-    tenant = Tenant.new(:name => params[:tenant][:name])
+    tenant = Tenant.new(:name => params[:name])
 
-    if new_signups_not_permitted?(params)
+    if new_signups_not_permitted?(coupon_params)
 
       raise ::Milia::Control::MaxTenantExceeded, "Sorry, new accounts not permitted at this time" 
 
@@ -581,6 +584,13 @@ export RECAPTCHA_PRIVATE_KEY=6LeBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBgQBv
 # *********************************************************************
 # STEP 6 - adding a tenanted members table, then inviting a member
 # *********************************************************************
+# remove any users, and tenants created above in STEP 5
+# start the rails console
+  $ rail c
+    > User.all.each{|x| x.destroy}
+    > Tenant.all.each{|x| x.destroy}
+    > exit
+
 
   $ rails g resource member tenant:references user:references first_name:string last_name:string favorite_color:string
 
@@ -602,12 +612,20 @@ export RECAPTCHA_PRIVATE_KEY=6LeBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBgQBv
 # REMOVE belongs_to :tenant
 # ADD
   acts_as_tenant
+
+  DEFAULT_ADMIN = {
+    first_name: "Admin",
+    last_name:  "Please edit me",
+    favorite_color: "blue"
+  }
+
+  def self.create_new_member(user, params)
+    # add any other initialization for a new member
+    return user.create_member( params )
+  end
+
   def self.create_org_admin(user)
-    new_member = user.create_member(
-      first_name: "First",
-      last_name:  "Admin",
-      favorite_color: "blue"
-    )
+    new_member = create_new_member(user, DEFAULT_ADMIN)
     unless new_member.errors.empty?
       raise ArgumentError, new_member.errors.full_messages.uniq.join(", ")
     end
@@ -616,11 +634,56 @@ export RECAPTCHA_PRIVATE_KEY=6LeBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBgQBv
       
   end
 
-# EDIT app/views/members/new.html.haml
 
-# EDIT app/models/user.rb
+# EDIT:  app/views/members/new.html.haml >>>>>>>>>>>>>>>>>>>>>>>>>>>>
+%h1 Simple Milia App
+.block#block-signup
+  %h2 Invite a new member into #{@org_name}
+  .content.login
+    .flash
+      - flash.each do |type, message|
+        %div{ :class => "message #{type}" }
+          %p= message
+    - flash.clear  # clear contents so we won't see it again
+
+    = form_for(@member, :html => { :class => "form login" }) do |f|
+      - unless @member.errors.empty? && @user.errors.empty?
+        #errorExplanation.group
+          %ul
+            = @member.errors.full_messages.uniq.inject(''){|str, msg| str << "<li> #{msg}"}.html_safe
+            = @user.errors.full_messages.uniq.inject(''){|str, msg| str << "<li> #{msg}"}.html_safe
+
+      = fields_for( :user ) do |w|
+        .group
+          = w.label :email, :class => "label "
+          = w.text_field :email, :class => "text_field"
+          %span.description Ex. test@example.com; must be unique
+
+      .group
+        = f.label :first_name, :class => "label "
+        = f.text_field :first_name, :class => "text_field"
+
+      .group
+        = f.label :last_name, :class => "label "
+        = f.text_field :last_name, :class => "text_field"
+
+      .group
+        = f.label :favorite_color, :class => "label "
+        = f.text_field :favorite_color, :class => "text_field"
+        %span.description What is your favorite color?
+
+      .group.navform.wat-cf
+        %button.button{ :type => "submit" }
+          = image_tag "web-app-theme/icons/key.png"
+          Create user and invite
+#<<<< ADD  <<<<<<<<<<<<<<<<<
+#<<<< EDIT <<<<<<<<<<<<<<<<<
+
+
+# EDIT app/models/user.rb >>>>>>>>>>>>>>>>>>>>
 # ADD
     has_one :member, :dependent => :destroy
+#<<<< EDIT <<<<<<<<<<<<<<<<<
 
 # EDIT app/controllers/application_controller.rb
 # ADD:
@@ -637,15 +700,67 @@ private
 
   end
 
-changes to layout for @org_name
-changes to layout for invite member
-members_controoller layouts
+# EDIT app/views/layouts/application.rb >>>>>>>>>>>>>>>>>>>>
+# the following is not a requirement, but serves to show
+# how to handle tenanted sign ins and welcome pages
+# replaces the two instances of "Simple Milia App" with 
+# (everything bewtween the quotes but not including the quotes):
+# "= @org_name", make the results look like the two lines below
+
+  %title= @org_name
+
+  = link_to @org_name, "/"
+
+# make changes to layout for invite member; change the portion 
+# with the "sign_up" link to look like the following:
+              %li
+                - if user_signed_in?
+                  = link_to t("web-app-theme.invite", :default => "Invite member"), new_member_path
+                - else
+                  = link_to( t("web-app-theme.signup", :default => "Sign up"), new_user_registration_path )
+
+#<<<< EDIT <<<<<<<<<<<<<<<<<
+
+# EDIT: app/controllers/members_controller.rb >>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+# ADD after the class line: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+  layout  "sign", :only => [:new, :edit, :create]
+
+  def new()
+    @member = Member.new()
+    @user   = User.new()
+  end
+
+  def create()
+    @user   = User.new( user_params )
+
+    # ok to create user, member
+    if @user.save_and_invite_member() && @user.create_member( member_params )
+      flash[:notice] = "New member added and invitation email sent to #{@user.email}."
+      redirect_to root_path
+    else
+      flash[:error] = "errors occurred!"
+      @member = Member.new( member_params ) # only used if need to revisit form
+      render :new
+    end
+
+  end
 
 
-NEED MILIA API EXPLAINED: Tenant.current_tenant, etc
+  private
 
-web-app-themes: add to generator: error_styles.sass
+  def member_params()
+    params.require(:member).permit(:first_name, :last_name, :favorite_color)
+  end
 
-user migration needs skip_confirm_change_password
-nt.rb first user needs skip_confirm_change_password <- true
+  def user_params()
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
+#<<<< ADD  <<<<<<<<<<<<<<<<<
+#<<<< EDIT <<<<<<<<<<<<<<<<<
+
+
+# TODO: NEED MILIA API EXPLAINED: Tenant.current_tenant, etc
 
