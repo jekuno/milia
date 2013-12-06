73450fbd7d20d2c5e18f3de9a6801d65bc198d00
daudi amani <dsaronin@gmail.com>
2013-12-06 00:50:15 -0800

    generators - 2

--- lib/generators/milia/initializer/initializer_generator.rb
+++ lib/generators/milia/initializer/initializer_generator.rb
@@ -1,10 +1,10 @@
 class InitializerGenerator < Rails::Generators::Base
   desc "Creates a milia initializer file in config/initializers"
 
+  source_root File.expand_path("../templates", __FILE__)
+  `
   def copy_initializer_file
-    copy_file 
-      File.expand_path('../templates/milia-initializer.rb', __FILE__)
-      "config/initializers/milia.rb"
+    copy_file 'initializer.rb', "config/initializers/milia.rb"
   end
 
 end
