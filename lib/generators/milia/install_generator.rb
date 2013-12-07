68838e7ab254ad9585fc2b253b71c68025dd5575
daudi amani <dsaronin@gmail.com>
2013-12-06 17:10:13 -0800

    generators - 8

--- lib/generators/milia/install_generator.rb
+++ lib/generators/milia/install_generator.rb
@@ -12,6 +12,22 @@ module Milia
         copy_file 'initializer.rb', 'config/initializers/milia.rb'
       end
 
+      def setup_devise
+        generate "devise:install"
+        generate "devise", "user"
+        gsub_file "app/models/user.rb", /,\s*$/, ", :confirmable,"
+        migrate_user_file = Dir.glob("db/migrate/[0-9]*_devise_create_users.rb").first
+        raise IOError, "devise db/migrate/xxxxxx_devise_create_users.rb not found" if migrate_user_file.blank? 
+        uncomment_lines( migrate_user_file, /confirm/ )
+        inject_into_file migrate_user_file,
+          after: "# t.datetime :locked_at\n" do <<-'RUBY'
+          
+          # milia member_invitable
+          t.boolean    :skip_confirm_change_password, :default => false
+          t.references :tenant
+        RUBY
+      end
+
     end  # class InstallGen
 
   end # module Gen
