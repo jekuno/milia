308c18e068519f63677b9b071d502539311f9a2e
daudi amani <dsaronin@gmail.com>
2014-01-25 01:09:43 -0800

    confirmations ctlr rewrite - 1

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,86 +1,71 @@
 # **************************************************************************
-# The basis for most of this code is from the work-around found on the devise wiki:
-# https://github.com/plataformatec/devise/wiki/How-To:-Override-confirmations-so-users-can-pick-their-own-passwords-as-part-of-confirmation-activation
-# with selected areas commented out for usage with milia's invite_member process
 # **************************************************************************
 
 module Milia
 
   class ConfirmationsController < Devise::ConfirmationsController
 
-    skip_before_action :authenticate_tenant!   #, :only => [:show, :new, :create]
+    skip_before_action :authenticate_tenant! 
 
 
   # PUT /resource/confirmation
+  # entered ONLY on invite-members usage to set password at time of confirmation
   def update
-    if ::Milia.use_invite_member
-      change_or_confirm_user( true )
-    else  # process as normal devise handling
-      super
-    end
+    if @confirmable.attempt_set_password(user_params)
+
+      # this section is patterned off of devise 3.2.5 confirmations_controller#show
+
+      self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+      yield resource if block_given?
+
+      if resource.errors.empty?
+        log_action( "invitee confirmed" )
+        set_flash_message(:notice, :confirmed) if is_flashing_format?
+          # sign in automatically
+        sign_in_and_redirect(resource_name, @confirmable)
+        
+      else
+        log_action( "invitee confirmation failed" )
+        respond_with_navigational(resource.errors, :status => :unprocessable_entity){ render :new }
+      end
+
+    else
+      log_action( "invitee password set failed" )
+      prep_do_show()  # prep for the form
+      respond_with_navigational(resource.errors, :status => :unprocessable_entity){ render :show }
+    end  # if..then..else passwords are valid
   end
 
   # GET /resource/confirmation?confirmation_token=abcdef
+  # entered on new sign-ups and invite-members
   def show
-    if ::Milia.use_invite_member
-      change_or_confirm_user( false )
-    else  # process as normal devise handling
-      super
+    unless ::Milia.use_invite_member && !@confirmable.skip_confirm_change_password
+      log_action( "devise pass-thru" )
+      super  # this will redirect 
+    else
+      log_action( "show password set form" )
+      prep_do_show()  # prep for the form
     end
+    # else fall thru to show template which is form to set a password
+    # upon SUBMIT, processing will continue from update
   end
   
   protected
-
-  def change_or_confirm_user(tryset=nil)
-      with_unconfirmed_confirmable do
-  #      if @confirmable.has_no_password?   # milea creates a dummy password when accounts are created
-        @confirmable.attempt_set_password(user_params) if tryset
-        is_valid = @confirmable.valid?
-        logger.debug(
-          "MILIA >>>>> [confirm] #{(tryset ? 'update' : 'show')}\tskip: #{@confirmable.skip_confirm_change_password}\tvalid: #{is_valid}"
-        ) unless logger.nil?
-          if ( @confirmable.skip_confirm_change_password && is_valid )
-            do_confirm   # user has a password; use it to sign in
-          else
-            do_show   # needs to create a password
-            @confirmable.errors.clear #so that we wont render :new
-          end
-  #       else
-  #         self.class.add_error_on(self, :email, :password_allready_set)
-  #       end
-      end
-
-      unless @confirmable.errors.empty?   # here if errors
-        self.resource = @confirmable
-        render :new
-      end
-
-  end
 
   def user_params()
     params.require(:user).permit(:password, :password_confirmation, :confirmation_token)
   end
 
-  def with_unconfirmed_confirmable
-    original_token = params[:confirmation_token]
-    confirmation_token = Devise.token_generator.digest(User, :confirmation_token, original_token)
-    @confirmable = User.find_or_initialize_with_error_by(:confirmation_token, confirmation_token)    
-    if !@confirmable.new_record?
-      @confirmable.only_if_unconfirmed {yield}
-    end
-  end
-
-  def do_show
+  def prep_do_show()
     @confirmation_token = params[:confirmation_token]
     @requires_password = true
     self.resource = @confirmable
-    render :show
   end
 
-  def do_confirm
-    @confirmable.confirm!
-    set_flash_message :notice, :confirmed
-    sign_in_and_redirect(resource_name, @confirmable)
+  def log_action( action )
+    logger.debug(
+      "MILIA >>>>> [confirm user] #{action} - #{@confirmable.email}"
+    ) unless logger.nil?
   end
 
   end  # class
