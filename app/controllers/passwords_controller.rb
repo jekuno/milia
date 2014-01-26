9d5caffeab431e0a7738e0e2152889af0b855e43
daudi amani <dsaronin@gmail.com>
2014-01-26 00:25:24 -0800

    paswords ctlr - 1

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -2,7 +2,7 @@ module Milia
 
   class PasswordsController < Devise::PasswordsController
 
-    skip_before_action :authenticate_tenant!, :only => [:new, :create ]
+    skip_before_action :authenticate_tenant!, :only => [:new, :create, :update ]
 
   end  # class
 end # module
