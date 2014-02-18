d9cb2362746cd7bb3610ac4149f1f7d5e65857e2
daudi amani <dsaronin@gmail.com>
2014-02-18 04:08:39 -0800

    revamp test - 24

--- test/test/controllers/home_controller_test.rb
+++ test/test/controllers/home_controller_test.rb
@@ -12,11 +12,26 @@ class HomeControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  should "get show" do
+  should "get show with login" do
     sign_in( users( :quentin ) )
     get :show
     assert_response :success
     sign_out( users( :quentin ) )
+  end  # should do
+
+  should 'not get show without login' do
+    assert_raise(ArgumentError, 'uncaught throw :warden'){
+      get :show
+    }
+    assert_response :success
+  end  # should do
+
+  should 'reset tenant' do
+    assert Tenant.current_tenant_id
+    @controller = ApplicationController.new
+    @controller.instance_eval{ __milia_reset_tenant! }   # invoke the private method
+    assert_nil Tenant.current_tenant_id
+
   end  # should do
 
   end  # context
