5dabd58ffaafa727b300ee2faa2e178010194059
daudi amani <dsaronin@gmail.com>
2014-02-19 18:37:46 -0800

    removed rake test override

--- test/db/schema.rb
+++ test/db/schema.rb
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 20111013050837) do
     t.datetime "updated_at"
   end
 
+  add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
