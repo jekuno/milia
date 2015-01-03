9db97620644ae3b66faaca74b73adc4445dfe078
daudi amani <dsaronin@gmail.com>
2015-01-02 18:21:05 -0800

    fix redirect loop issues - 2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,10 @@ module Milia
 
   class SessionsController < Devise::SessionsController
 
-       # clear tenanting
-    before_action :__milia_reset_tenant!, :only => [:create, :destroy]
        # skip need for authentication
     skip_before_action :authenticate_tenant!, :only => [:new, :create, :destroy]
+       # clear tenanting
+    before_action :__milia_reset_tenant!, :only => [:create, :destroy]
 
     def create
       Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
