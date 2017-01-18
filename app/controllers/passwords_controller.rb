0d619ad71ac45bc5166d9a8bf85e78a71886a8fe
jekuno <jekuno@users.noreply.github.com>
2017-01-18 21:37:38 +0100

    Don't raise on missing callback

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -2,7 +2,7 @@ module Milia
 
   class PasswordsController < Devise::PasswordsController
 
-    skip_before_action :authenticate_tenant!, :only => [:new, :create, :edit, :update ]
+    skip_before_action :authenticate_tenant!, :only => [:new, :create, :edit, :update ], raise: false
 
   end  # class
 end # module
