72ec8dd8c2671e2f73df8b584d90690a8258c50f
jekuno <jekuno@users.noreply.github.com>
2017-01-18 21:38:54 +0100

    Don't raise on missing callback

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,9 @@ module Milia
 
   class SessionsController < Devise::SessionsController
        # skip need for authentication
-    skip_before_action :authenticate_tenant!, :only => [:new, :create, :destroy]
+    skip_before_action :authenticate_tenant!, :only => [:new, :create, :destroy], raise: false
        # clear tenanting
-    before_action :__milia_reset_tenant!, :only => [:create, :destroy]
+    before_action :__milia_reset_tenant!, :only => [:create, :destroy], raise: false
 
   end  # class
 end # module
