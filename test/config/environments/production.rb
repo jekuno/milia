705b5ecb928b1989a42fae14c0e2bf7d382367a0
jekuno <jekuno@users.noreply.github.com>
2017-01-27 12:58:09 +0100

    Updates binstubs and config

--- test/config/environments/production.rb
+++ test/config/environments/production.rb
@@ -35,7 +35,7 @@ Miliatest::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_files = false
+  config.public_file_server.enabled = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
